9209351903ff6a4be87352dcb3d711fc70b22419
Taive26 <73930187+Taive26@users.noreply.github.com>
2021-02-22 14:41:19 +0200

    Delete db.js

--- database/src/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = global.Promise
-
-module.exports = {
-    Users: require('./models/users.model')
-}
